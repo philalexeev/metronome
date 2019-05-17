8d774f869090d8753a40dc17b231b6f170d829ca
philalexeev <phill08@mail.ru>
2019-05-17 12:58:09 +0300

    all done. need to add vue-long-click plugin

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,12 @@ let app = new Vue({
 		temp: 80,
 		onAir: false,
 		btnName: 'play',
-		audio: new Audio()
+		timerId: 0
+	},
+	computed: {
+		tempTime: function () {
+			return ( 60 / this.temp ) * 1000
+		}
 	},
 	methods: {
 		faster() {
@@ -16,18 +21,20 @@ let app = new Vue({
 			this.temp--;
 		},
 		btnClick() {
+			let audio = new Audio();
 			if (this.onAir === false) {
 				this.onAir = true;
 				this.btnName = 'stop';
 				audio.src = '../misc/click.ogg';
-				audio.autoplay = true;
-				audio.loop = true;
+				audio.play();
+				this.timerId = setInterval(() => {
+					audio.play();
+				}, this.tempTime);
 			} else {
 				this.onAir = false;
 				this.btnName = 'play';
 				audio.pause();
-				audio.currentTime = 0;
-				audio.loop = false;
+				clearInterval(this.timerId);
 			}
 		}
 	}
